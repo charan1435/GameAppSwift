7aa5893f7855029e8a8992e20b906be6f912a206
charan1435 <sc16101@gmail.com>
2024-11-23 02:18:45 +0530

    layout and tab view

---
+++ GameApp/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GameApp
+//
+//  Created by Sabith Salman on 2024-11-23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
