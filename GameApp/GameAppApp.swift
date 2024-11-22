74ed97b794a195412910c03d01a01f423677ca34
charan1435 <sc16101@gmail.com>
2024-11-23 01:58:56 +0530

    Initial Commit

---
+++ GameApp/GameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppApp.swift
+//  GameApp
+//
+//  Created by Sabith Salman on 2024-11-23.
+//
+
+import SwiftUI
+
+@main
+struct GameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameAppTests/GameAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameAppTests.swift
+//  GameAppTests
+//
+//  Created by Sabith Salman on 2024-11-23.
+//
+
+import Testing
+@testable import GameApp
+
+struct GameAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
