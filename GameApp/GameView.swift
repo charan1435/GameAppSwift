0d48c9fe59cecdfe21e169c98faed3f9554d6cc9
charan1435 <sc16101@gmail.com>
2024-11-23 02:55:57 +0530

    game view 2nd logic

--- GameApp/GameView.swift
+++ GameApp/GameView.swift
@@ -15,24 +15,38 @@ enum MathOperators: String, CaseIterable{
 }
 
 struct GameView: View {
-    @State var answer: String = ""
+    
+    @State var mathOperator: MathOperators = MathOperators.allCases.randomElement()!
+    @State var firstNumber: Int = Int.random(in: 0...9)
+    @State var secondNumber: Int = Int.random(in: 0...9)
+    
+    @State var answer: Int? = nil
     @State var points: Int = 0
     var body: some View {
         VStack{
             Text("Guess the answer")
-            Text("What is x + 1 = ?")
+                .foregroundStyle(Color(.green))
+            Text("What is \(firstNumber)\(mathOperator.rawValue)\(secondNumber)")
+                .font(.largeTitle)
+                .bold()
             HStack{
-                TextField("Enter answer here",text: $answer)
+                TextField("Enter answer here",value : $answer , format: .number)
                 Button("Submit"){
                     
                 }
+                .buttonStyle(.bordered)
+                .tint(.blue)
             }
+            .padding()
+            .border(.black,width: 1)
             
             Text("\(points)")
                 .font(.system(size:150))
             Button("Next"){
                 
             }
+            .buttonStyle(.bordered)
+            .tint(.green)
             
             
             
