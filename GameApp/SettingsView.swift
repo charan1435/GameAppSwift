33225f52dd651a004c9ff6c91511139bb52f4517
charan1435 <sc16101@gmail.com>
2024-11-23 23:22:28 +0530

    Settings UI and Logic

--- GameApp/SettingsView.swift
+++ GameApp/SettingsView.swift
@@ -7,11 +7,55 @@
 
 import SwiftUI
 
-struct SettingsView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+enum AppTheme : String,CaseIterable{
+    case red = "Red"
+    case green = "Green"
+    case orange = "Orange"
 }
+
+struct SettingsView: View {
+    @State var fontSize: Double = 20.0
+    
+    @State var selectedColor : Color = .red
+    
+    let colors: [Color] = [.red,.green,.orange]
+    let colorNames: [String] = ["Red", "Green", "Orange"]
+    
+    var body: some View {
+        NavigationStack{
+            
+            Slider(value: $fontSize, in: 0...100, step: 1){
+                
+            }
+            Text("font size \(String(format: "%.2f", fontSize))")
+            HStack{
+                Text("Select system colour")
+                Picker("", selection: $selectedColor){
+                    ForEach(AppTheme.allCases, id: \.self){
+                        theme in
+                        Text(theme.rawValue)
+                            .tag(theme.rawValue)
+                    }
+                            
+                    }
+                .pickerStyle(.wheel)
+                Color(selectedColor)
+                    .frame(width: 30, height: 30)
+                }
+            }
+            
+            
+            
+                
+            
+            
+            
+                .navigationTitle("Settings")
+            
+            
+        }
+    }
+
 
 #Preview {
     SettingsView()
